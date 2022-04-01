20033d0b4dd04e7a9673686385ae10b532a55262
Anderson Dias <andersoninfonet@gmail.com>
2022-04-01 00:33:17 -0300

    player module and guard configuration

---
+++ src/app/pages/player/player.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { PlayerComponent } from "./player/player.component";
+
+export const PlayerRotas: Routes = [
+  {
+    path: '',
+    component: PlayerComponent
+  }
+]
