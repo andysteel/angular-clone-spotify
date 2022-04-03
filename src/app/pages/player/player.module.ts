597ef10711c11682f6cdd8dc6eee3df741878d79
Anderson Dias <andersoninfonet@gmail.com>
2022-04-02 22:54:28 -0300

    painel esquerdo init

--- src/app/pages/player/player.module.ts
+++ src/app/pages/player/player.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { PlayerRotas } from './player.routes';
 import { PlayerComponent } from './player/player.component';
+import { PainelEsquerdoComponent } from 'src/app/components/painel-esquerdo/painel-esquerdo.component';
 
 
 @NgModule({
   declarations: [
-    PlayerComponent
+    PlayerComponent,
+    PainelEsquerdoComponent
   ],
   imports: [
     CommonModule,
