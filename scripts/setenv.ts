0fe99e36efff380461d9efd9b822ef8102e2f74a
Anderson Dias <andersoninfonet@gmail.com>
2022-04-09 21:15:24 -0300

    configuring env variables in build time

---
+++ scripts/setenv.ts
@@ -0,0 +1,46 @@
+const { writeFile } = require('fs');
+const { argv } = require('yargs');
+// read environment variables from .env file
+require('dotenv').config();
+// read the command line arguments passed with yargs
+const environment = argv.environment;
+const isProduction = environment === 'prod';
+
+if (!process.env['SPOTIFY_CLIENT_ID']) {
+  console.error('All the required environment variables were not provided!');
+  process.exit(-1);
+}
+
+const targetPath = isProduction
+   ? `./src/environments/environment.prod.ts`
+   : `./src/environments/environment.ts`;
+// we have access to our environment variables
+// in the process.env object thanks to dotenv
+const environmentFileContent = `
+export const environment = {
+   production: ${isProduction},
+};
+
+export const SpotifyConfiguration = {
+  clientID: '${process.env['SPOTIFY_CLIENT_ID']}',
+  authEndpoint: 'https://accounts.spotify.com/authorize',
+  redirectUrl: 'http://localhost:4200/login/',
+  scopes: [
+    'user-read-currently-playing', // musica tocando agora
+    'user-read-recently-played', // ler musicas tocadas recentemente
+    'user-read-playback-state', // ler estado do player do usuario
+    'user-top-read', // top artistas e musicas do usuario
+    'user-modify-playback-state', // alterar player do usuario
+    'user-library-read', // ler biblioteca do usuario
+    'playlist-read-private', // ler playlists privadas
+    'playlist-read-collaborative'// ler playlists abertas
+  ]
+}
+`;
+// write the content to the respective file
+writeFile(targetPath, environmentFileContent, function (err: any) {
+   if (err) {
+      console.log(err);
+   }
+   console.log(`Wrote variables to ${targetPath}`);
+});
